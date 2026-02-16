55f9651b400566e4254cee2c509c10c6e8c6bf21
ex, michael <eag1ex@protonmail.com>
2026-02-16 18:15:38 +1300

    adjust navigation without sceleton

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Stacks app
+# Stacks App
 
 Stack listing app build with Svelte, it allows you to perform all CRUD operations, it runs on mock server `miragejs`
 
@@ -10,7 +10,14 @@ Stack listing app build with Svelte, it allows you to perform all CRUD operation
 - Good separation co concerns (SOC)
 - State management integrated with service api
 - Passing tests
-- tslint and prettie
+- tslint and prettier clean and valid
+- mock API endpoints with miragejs
+- SvelteKit integration
+- environment ready with dotenv
+
+## Stack
+
+- Svelte, miragejs, Typescript, CRUD, RestApi, Tailwindcss, css/scss,
 
 ## Testing
 
@@ -20,3 +27,8 @@ Running tests with miragejs mock server requires jsdom with headless browser:
 
 on windows it should install it under:
 `C:\Users\<username>\AppData\Local\ms-playwright\chromium-<version>\`
+
+## Requirements
+
+- need Node version ^20.19.0
+- need to install chromium for testing
--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 	import { appNavigation } from '@/data/navigation';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
+
+	// Track the current tab value (optional for active state)
 	export let currentTab: any;
 
 	const handleTabClick = (slug: any) => {
 		if (appNavigation.map((n) => n.slug).includes(slug)) {
-			goto(resolve(slug));
+			goto(resolve(slug)); // Navigate to the selected page
 		} else {
-			goto(resolve('/404' as any));
+			goto(resolve('/404' as any)); // Navigate to 404 page if the slug is not valid
 		}
 	};
 </script>
 
-<Tabs class="navigation mt-4 w-full" on:change={handleTabClick}>
-	<Tabs.List class="flex w-full justify-center gap-4">
+<div class="navigation my-4 w-full">
+	<div class="flex w-full justify-center gap-4">
 		{#each appNavigation as { slug, title }}
-			<Tabs.Trigger value={slug} class={`tab text-lg ${$currentTab === slug ? 'active-tab' : ''}`}>
-				<span on:click|preventDefault={() => handleTabClick(slug)} class="block py-1 text-center"
-					>{title}</span
-				>
-			</Tabs.Trigger>
+			<a
+				href={slug}
+				on:click={() => handleTabClick(slug)}
+				class={`tab rounded p-2 text-lg ${$currentTab === slug ? 'active-tab border' : ''}`}
+			>
+				{title}
+			</a>
 		{/each}
-	</Tabs.List>
-</Tabs>
+	</div>
+	<div class=" mt-4 border-t-1 border-solid border-[var(--bg-color-reverse)]"></div>
+</div>
